ac91a099869dfb5c19f3c507391073c322b8470e
pandaxcore <pandadesign7@gmail.com>
2024-04-12 22:01:11 +0500

    attempt III

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+
 from . import views
 
 
-# Указываем namespace для ссылок приложения:
 app_name = "blog"
 
 
--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
+
 from . import views
 
 
-# Указываем namespace для ссылок приложения:
 app_name = 'pages'
-
 
 urlpatterns = [
     path('about/', views.about, name='about'),
